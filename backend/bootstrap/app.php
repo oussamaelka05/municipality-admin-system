9038751f30e56b7ca6787a3ee87564b16afe1049
ElkasmiOussama <elkasmioussama05l@gmail.com>
2026-06-17 22:17:43 +0100

    Update backend and frontend

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,11 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
